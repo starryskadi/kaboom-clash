3a8b1d3935f2996c26547763c20bdb9bd9a27897
Starry Skadi <starryskadi@gmail.com>
2024-10-26 01:23:24 +0700

    feat: add character & its movement

---
+++ scene/player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_vector("ui_left", "ui_right", 'ui_up', 'ui_down')
+	
+	velocity = direction * SPEED
+
+	move_and_slide()
